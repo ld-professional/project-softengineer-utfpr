f786e92b0aa7bdc9ff7ec44ffe772ec99c310708
Lucasl_local_pc <lucas@hotmail.com>
2025-11-19 00:51:39 -0300

    criando o amdin de account

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
-
+from .models import UserPersonalizado
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+
+#. Como você está usando um AbstractUser, o ideal não é usar admin.ModelAdmin simples, e sim herdar de UserAdmin.
+
+class UserPersonalziadoAdmin(UserAdmin):
+
+    list_display=(
+        'username',
+        'email',
+        'telefone',
+        #'is_staff' so se eu tivesse funcionarios...
+        #  mas o mehlor seria criar um campo proprio chamado eh_funct pq aqui eles tem acesso
+        #  ao /admin msm q seja so pra visualizar
+    )
+
+    search_fields=(
+        'username',
+    )
+
+    list_filter=(['is_staff', 'is_superuser', 'is_active'])
+
+
+    #clico em useradmin copio um tal de  fieldsets e em informacoes pessoas coloco o telefone
+    # cltr e botao direito em useradmin => go to definiton, dps apagar o _()
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ("first_name", "last_name", "email",'telefone')}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
+
+admin.site.register(UserPersonalizado,UserPersonalziadoAdmin)
--- account/models.py
+++ account/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-# Create your models here.
+
 # tem q ter 2 classes auqi pq o django separa a lgoica de ciracao da logica de estrutura de dados
 #  so precisamos criar a fabrica, o gerente, por causa do campo senha, em auth_user, difernete dos outros
 #modelos, o quais ja vem com o gerente padrao do django...
 
 
 #onde o trabalho eh simpels, o objects no caso.. ,receber os dados e salvar exatamente oq recebe
+# basicamente como no meu docs, serve para fazer a criacao do objeto user, mas 
+# n seria necessario, mas criamos um so pq temos q ter q fazer a senha apssar por uma hash de seguranca
+# e logo tbm pdoeriamos n ter as validacoes aqui poderia ser feito pelo proprio forms.py mas
+# aqui eh executado antes entao eh meio q devemos fazer as validacoes aqui tbm!
 
-class criador_user(BaseUserManager):
+class CriadorUser(BaseUserManager):
 
     def create_user(self,username,email,telefone,password=None):
 
@@ -22,12 +26,75 @@ class criador_user(BaseUserManager):
         if not username:
             raise ValueError('O campo Username é obrigatório.')
         if not telefone:
-            raise ValueError('O campo CPF é obrigatório.')
+            raise ValueError('O campo telefone é obrigatório.')
         
         # Normaliza o email (transforma letras maiúsculas em minúsculas no domínio)
         email = self.normalize_email(email)
 
-        telefone_stringado= str(self.telefone)
+        telefone_stringado= str(telefone) # n eh self.telefone pq telefone eh do objeto user e nao somos o user, e a
+                                          # tbm a questao q ele entra ocmo arugmetno, auqi ja estamos pegando o self.telefone 
+ 
+        if len(telefone_stringado) != 11:
+            raise ValueError({'telefone':'Digite um numero valido 43 91234 5678'})
+        
+        # 4. Criação do Objeto
+        user = self.model(
+            username=username,
+            email=email,
+            telefone=telefone
+        )
 
-        if telefone_stringado.count() >11
-            raise ValueError({'telefone':'O campo telefone necessita ser de 11 digitos'})
+        # 5. Segurança da senha e Salvamento do objeto
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, username, email,telefone, password=None):
+        # O create_superuser precisa receber os argumentos que o create_user exige
+        # O terminal vai pedir username, email e password. 
+
+        
+        if not telefone:
+             raise ValueError('Superusuário precisa de telefone tambem.')
+
+        user = self.create_user(
+            username=username,
+            email=email, 
+            telefone=telefone,
+            password=password
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    
+
+class UserPersonalizado(AbstractUser):
+# Troquei para charfild para não perder o zero à esquerda (ex: 011...)
+    telefone = models.CharField(max_length=11, unique=True) # campo q deve ser unico tbm sozinho obvio para este caso
+    # apesar de ser nao aceitar nulo, nao impede de tentarem salvar None aqui, entao devemos fazer uma validacao
+    # ou seja obviametne seria o forms.py mas n tem enfim sera no criador...
+
+    # Configuração para o comando 'createsuperuser' no terminal
+    # O 'username' já é o padrão do AbstractUser, então não precisa listar aqui.
+
+    # Listamos apenas os CAMPOS EXTRAS que o terminal deve pedir.
+
+    REQUIRED_FIELDS = ['email','username'] #'telefone'], LOGO como ja tem telefone para ser o principal, aqui deve ser email e username
+    # lista de pgntas extras pro temrinal criar um superuser em: python manage.py createsuperuser
+
+    USERNAME_FIELD = 'telefone' # na tela de login do /admin USAR telefone para login!
+
+    def __str__(self):
+
+        return f'User: {self.username}, email: {self.email}'
+    
+
+    # --- A CONEXÃO OBRIGATÓRIA ---
+    # Diz ao modelo para usar o meu gerente/ fabrica q eh o craidorUser
+    objects = CriadorUser() 
+
+
+    #A Conexão: Ao escrever objects = CriadorUser(), você está dizendo:
+    #  "Django, quando alguém chamar User.objects.create_user, não use o método padrão do Django.
+    #  Use o método personalizado que EU escrevi na classe CriadorUser (aquele que verifica os 11 dígitos)."
