a0c3b17bc4cec41ece472fa22ffce3fe55667321
LUIS HENRIQUE <lhfspro@gmail.com>
2025-11-19 15:35:59 -0300

    feat:estrutura inicial

--- agendamentos/views.py
+++ agendamentos/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
-class cliente_realizar_agendamento():
-    pass
+
+
+
+def visualizar_horarios(request):
+
+    #verificar se é .get ou .post
+
+    #converter json para dictpy
+
+    #criar todos os horários possíveis
+
+    #verificar ORM de cada objeto barbeiro, horarios e fazer uma lista com todos os horários em que vai ter um barbeiro trabalhando
+
+    #depois de fazer um and entre as 2 listas, é preciso fazer a verificação de exceções, na qual a tabela exceções será verificada e todos os horários inclusos lá serão tirados da lista
+
+    #verificar overlap de serviços
+
+    #reconstruir o json
+
+    #enviar o json
+
+    return 0,
+
+def agendar_horario(request):
+
+    #converter o json para dictpy
+
+    #criar um objeto na tabela agendamentos e definir fk para os outros modelos
+
+    #retornar o json informando sucesso
+
+    return 0,
