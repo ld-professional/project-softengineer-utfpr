a9fb2b6fce80052e581e90e8d3d8435d09bead5b
LUIS HENRIQUE <lhfspro@gmail.com>
2025-11-19 23:22:50 -0300

    feat:tela_disponibilidade_slots_barbeiro

--- agendamentos/models.py
+++ agendamentos/models.py
@@ -6,7 +6,6 @@ from servicos.models import Servicos
 from django.core.exceptions import ValidationError
 from datetime import timedelta
 
-# Create your models here.
 class Agendamentos(models.Model):
 
     id_agendamentos= models.AutoField(primary_key=True)
@@ -14,55 +13,34 @@ class Agendamentos(models.Model):
     fk_cliente= models.ForeignKey(Cliente,on_delete=models.CASCADE)
     fk_barbeiro = models.ForeignKey(Barbeiro,on_delete=models.CASCADE)
     fk_servicos= models.ForeignKey(Servicos,on_delete=models.CASCADE)
-    data_e_horario_inicio = models.DateTimeField() # pode ser chamado de data + hora inicio
-    data_e_horario_fim= models.DateTimeField(blank=True,null=True,editable=False) # sua logica/ salvamento eh feito no back end
-                                                # usando data_fim= fk_serivcos_slot_duracao_servico *30
-                                                # mas devo permitir o campo ser nulo a priori so pras primeiras
-                                                #verificacoes de integridade de cada coluna nao falhas
-                                                # pois de qlqer forma, na hora de salvar os dados no bd, ele salva todos
-                                                # as colunas ao msm tempo, neste caso
-                                                #editable eh um campo q so em view.py pode fazer adicioanr dado nesta ocluan
-                                                # logo usuario em si, nem o admin cosnegue ver este campo
+    data_e_horario_inicio = models.DateTimeField()
+    data_e_horario_fim= models.DateTimeField(blank=True,null=True,editable=False)
     class Meta:
         constraints=[models.UniqueConstraint(fields=[
-            #'fk_cliente'                
-            'fk_barbeiro',              # esta seria a pk pois num agendamento eh unico para cada linha o barbeiro + o exato momento
-            'data_e_horario_inicio',    # mas o cliente tbm eh unico para cada data logo outra constraint
-            ],
-            name='agendamento_unico_por_barbeiro'
-        ),
+            'fk_barbeiro',
+            'data_e_horario_inicio'],
+            name='agendamento_unico_por_barbeiro'),
+
         models.UniqueConstraint(fields=[
             'fk_cliente',       
-            #'fk_barbeiro',             
-            'data_e_horario_inicio',   
-            ],
+            'data_e_horario_inicio'],
             name='agendamento_unico_por_cliente',)
         ]
 
     def __str__(self):
         data_formatada = self.data_e_horario_inicio.strftime('%d/%m/%Y %H:%M')
     
-    # para o caso de fazer qlqer coisa com o objeto antes msm dele ser asavlo no bd
         if self.data_e_horario_fim: 
             data_fim_formatada = self.data_e_horario_fim.strftime('%d/%m/%Y %H:%M')
             return f'''Cliente:{self.fk_cliente.fk_user.username}, com o barbeiro: {self.fk_barbeiro.fk_user.username},em:\n
                     {data_formatada} até {data_fim_formatada}.
                 '''
     
-        # se ele realmente ainda n foi salvo, logo usa este retorno
         return f'''Cliente:{self.fk_cliente.fk_user.username}, com o barbeiro: {self.fk_barbeiro.fk_user.username},em:\n
                 {data_formatada} (calculando fim...)
         '''
 
     def clean(self):
-    # lembrando q eh um metodo para validacao e nao salvamento... 
-    # mei oq um meio termo sobre forms
-    # ou seja, as variaveis vidnas do front atraves do json, serao passdas pra um forms
-    # o qual tal forms salva cada variavel do json a uma acossaiada a um objeto do tipo agendamento
-    # e no final chama este metodo clean
-    # e dpeosi de chamar o clea, o useja o clean passar signfica q ta tudo ok
-    # e se tiver tudo ok, ele entao salva no banco de dados
-    # e dai no banc ode dados antes tbm por baixo dos panos faz as validacoes da class meta cosntraints ali...
         super().clean()
 
         duracao_em_minutos = self.fk_servicos.slot_duracao_servico * 30 
@@ -80,8 +58,4 @@ class Agendamentos(models.Model):
             conflitos = conflitos.exclude(pk=self.pk)
         
         if conflitos.exists():
-            raise ValidationError(
-                f'Conflito de horário! Ja existe um agendamento ocorrendo neste slot de horario!'
-            )
-
-        # ainda falta toda logica do agendamento mas isso sera feito como parte do bac-end para agendamentos...
+            raise ValidationError(f'Conflito de horário! Ja existe um agendamento ocorrendo neste slot de horario!')
--- agendamentos/views.py
+++ agendamentos/views.py
@@ -1,15 +1,73 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.db import transaction
+from django.core.exceptions import ValidationError
+from datetime import datetime, timedelta, date
+import json
+from barbeiro.models import Barbeiro, Horarios_de_trabalho, Excecoes
+from servicos.models import Servicos
+from clientes.models import Cliente
+from .models import Agendamentos
 
 
+@login_required
+def api_ver_horarios(request):
+
+    if request.method != 'GET':
+        return JsonResponse({'error': 'Método não permitido.'}, status=405)
+
+    data_busca = request.GET.get('data') 
+    servico_id = request.GET.get('servico_id')
+        
+    if not all([data_busca, servico_id]):
+        return JsonResponse(
+            {'error': 'Parâmetros de busca insuficientes: data, cliente e serviço são obrigatórios.'}, status=400)
+     
+    try:
+        cliente = Cliente.objects.get(fk_user=request.user)
+        cliente_id = cliente.pk
+        data_escolhida = datetime.strptime(data_busca, '%Y-%m-%d').date()
+        servico = Servicos.objects.get(pk=servico_id)
+    
+    except (ValueError, Servicos.DoesNotExist, Cliente.DoesNotExist):
+        return JsonResponse({'error': 'Dados inválidos ou cliente/serviço não encontrado.'}, status=400)
+
+    duracao_servico_minutos = servico.slot_duracao_servico * 30
+
+    disponibilidade_por_barbeiro = {}
+
+    todos_barbeiros = Barbeiro.objects.all()
+
+    if not todos_barbeiros.exists():
+        return JsonResponse({'message': 'Nenhum barbeiro cadastrado.'})
+
+    for barbeiro_obj in todos_barbeiros:
+        dia_semana_num = data_escolhida.weekday()
+
+        turnos_base = Horarios_de_trabalho.objects.filter(
+            fk_barbeiro=barbeiro_obj,
+            dia_semana=dia_semana_num
+        )
+        if not turnos_base.exists():
+            continue
+        
+        slots_do_dia = []
+
+        for turno in turnos_base:
+            
+            hora_inicio_turno = datetime.combine(data_escolhida, turno.hora_inicio)
+            hora_fim_turno = datetime.combine(data_escolhida, turno.hora_fim)
+            
+            slot_atual = hora_inicio_turno
+            
+            while slot_atual < hora_fim_turno:
+                slots_do_dia.append(slot_atual)
+                slot_atual += timedelta(minutes=30)
+
+        pass
 
 
-def visualizar_horarios(request):
-
-    #verificar se é .get ou .post
-
-    #converter json para dictpy
-
-    #criar todos os horários possíveis
 
     #verificar ORM de cada objeto barbeiro, horarios e fazer uma lista com todos os horários em que vai ter um barbeiro trabalhando
 
@@ -21,7 +79,8 @@ def visualizar_horarios(request):
 
     #enviar o json
 
-    return 0,
+    # Ação: Substituir o 'return 0,'
+    return JsonResponse({'message': 'Algoritmo finalizado. Necessita do JSON de saída.'}, status=200)
 
 def agendar_horario(request):
 
