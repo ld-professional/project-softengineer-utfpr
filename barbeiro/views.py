3a42b5f7e1f06112896e79db90b4b92bb88c9b6e
Lucasl_local_pc <lucas@hotmail.com>
2025-11-20 17:25:07 -0300

    bugfixes: barbeiro/ rotas e dashboard inicial arrumadas

--- barbeiro/views.py
+++ barbeiro/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
+@login_required(login_url='/account/login/')
+def barbeiro_dashboard(request):
+        
+    if  hasattr(request.user, 'clientao'):
+        return redirect('/clientes/dashboard/')
+
+    if not hasattr(request.user, 'barber'):
+        # Se não for cliente (nem barbeiro),
+        # mandamos ele para a home ou para o painel dele.
+        return redirect('/')
+
+
+    if request.method == 'GET':
+        return render(request,'barbeiro/dashboard.html') # n pode ter barra !
