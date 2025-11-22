563470823b6ab1c9a54559c47e58257cbbde957e
Lucasl_local_pc <lucas@hotmail.com>
2025-11-22 13:24:02 -0300

    ajustado rotas que existem no momento

--- agendamentos/urls_barbeiro.py
+++ agendamentos/urls_barbeiro.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
 
-path('minha-agenda/',views.barbeiro_visualizar_agenda,name='barbeiro_ver_agendamento'),
+#path('minha-agenda/',views.barbeiro_visualizar_agenda,name='barbeiro_ver_agendamento'),
 
-path('editar-agenda/',views.barbeiro_editar_agenda,name='barbeiro_editar_agendamento'),
+#path('editar-agenda/',views.barbeiro_editar_agenda,name='barbeiro_editar_agendamento'),
 ]
