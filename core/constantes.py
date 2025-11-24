185c0e72e6c123cde97da1979ea4f3d850e3375f
Lucasl_local_pc <lucas@hotmail.com>
2025-11-24 00:39:26 -0300

    feat: constante.py novas rotas

--- core/constantes.py
+++ core/constantes.py
@@ -7,3 +7,9 @@ LOGIN= 'account/login.html'
 CADASTRO= 'account/signup.html'
 
 BARBEIRO_DASHBOARD= 'barbeiro/dashboard.html'
+
+ESCOLHER_SERVICO= 'agendamento/agendamento.html'
+
+ESCOLHER_BARBEIRO = 'agendamento-barber.html'
+
+ESCOLHER_DIA= 'agendamento-horario.html'
