84ce8ef017fdec83e996c42231de487d541e2f14
Lucasl_local_pc <lucas@hotmail.com>
2025-11-24 01:55:55 -0300

    refac: rota agendamentos

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-#path('agendamentos/',include('agendamentos.urls_clientes')),
+path('agendamentos/',include('agendamentos.urls_clientes')),
 
 path('dashboard/',views.cliente_dashboard,name='clientepagina_inicial_dashboard'),
 ]
